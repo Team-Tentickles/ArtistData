32ddee63cf123be5e91b328c4fd76efd93ba49f0
KyleEvan <kep6634@rit.edu>
2016-04-05 16:14:40 -0400

    second commit

--- artists.js
+++ artists.js
@@ -1,736 +1,317 @@
 var artists = {
-  "1950s": [
-    {
-      "name": "Elvis Presley",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Perry Como",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Eddie Fisher",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Patti Page",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Nat King Cole",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Pat Boone",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Frankie Laine",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Frank Sinatra",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Guy Mitchell",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "The Four Aces",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Fats Domino",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Kay Starr",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Les Paul & Mary Ford",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Jo Stafford",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Doris Day",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "The Platters",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Ricky Nelson",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "The Ames Brothers",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Teresa Brewer",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Bill Haley & His Comets",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    }
-  ],
-  "1960s": [
-    {
-      "name": "The Beatles",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "The Supremes",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "The Rolling Stones",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "The Beach Boys",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Brenda Lee",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Ray Charles",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Roy Orbison",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Connie Francis",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "The Four Seasons",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Cliff Richard",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "The Everly Brothers",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "The Temptations",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Bobby Vinton",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Herman’s Hermits",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Chubby Checker",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "James Brown",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Stevie Wonder",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "The Dave Clark Five",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Aretha Franklin",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Petula Clark",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    }
-  ],
-  "1970s": [
-    {
-      "name": "Paul McCartney",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Bee Gees",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Elton John",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "The Carpenters",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Jackson 5",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Rod Stewart",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "ABBA",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Donna Summer",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Chicago",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Olivia Newton-John",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Glady’s Knight & the Pips",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "John Denver",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Diana Ross",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Neil Diamond",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Dawn",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Eagles",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "The Stylistics",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "The Commodores",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Barry Manilow",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Earth, Wind & Fire",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    }
-  ],
-  "1980s": [
-    {
-      "name": "Madonna",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Prince",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Michael Jackson",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Hall & Oates",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Duran Duran",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Kool & the Gang",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Phil Collins",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Lionel Richie",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Whitney Houston",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Billy Joel",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Huey Lewis & the News",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Bruce Springsteen",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "John Mellencamp",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "George Michael",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "The Police",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Billy Ocean",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Culture Club",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Genesis",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Wham!",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "The Pointer Sisters",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    }
-  ],
-  "1990s": [
-    {
-      "name": "Mariah Carey",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Janet Jackson",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Boyz II Men",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Celline Dion",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "TLC",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "R. Kelly",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Bryan Adams",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Michael Bolton",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "U2",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "The Spice Girls",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "R.E.M.",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Toni Braxton",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Ace of Base",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Monica",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Backstreet Boys",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "En Vogue",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "2Pac",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Take That",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Pearl Jam",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "SWV",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    }
-  ],
-  "2000s": [
-    {
-      "name": "Beyonce",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Rihanna",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Britney Spears",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Eminem",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Usher",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Nelly",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Jennifer Lopez",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "The Black Eyed Peas",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "50 Cent",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Pink",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Mariah Carey",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Kanye West",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Kelly Clarkson",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Nickelback",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Justin Timberlake",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Destiny’s Child",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Alicia Keys",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Madonna",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Lady Gaga",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    },
-    {
-      "name": "Jay-Z",
-      "images": [],
-      "videos": [],
-      "influencers": []
-    }
-  ]
+	"decade": {
+		"year": "1950s",
+		"artistsArray": [
+			{
+				"name": "Elvis Presley",
+				"bio": [],
+				"images": [],
+				"videos": [],
+				"songs": [],
+				"influencers": []
+			},
+			{
+				"name": "Frank Sinatra",
+				"images": [],
+				"videos": [],
+				"influencers": []
+			},
+			{
+				"name": "Nat King Cole",
+				"bio": [],
+				"images": [],
+				"videos": [],
+				"songs": [],
+				"influencers": []
+			},
+			{
+				"name": "Johnny Cash",
+				"bio": [],
+				"images": [],
+				"videos": [],
+				"songs": [],
+				"influencers": []
+			},
+			{
+				"name": "Ella Fitzgerald",
+				"bio": [],
+				"images": [],
+				"videos": [],
+				"songs": [],
+				"influencers": []
+			}
+		]
+	},
+	"decade": {
+		"year": "1960s",
+		"artistsArray": [
+			{
+				"name": "The Beatles",
+				"bio": [],
+				"images": [],
+				"videos": [],
+				"songs": [],
+				"influencers": []
+			},
+			{
+				"name": "The Supremes",
+				"bio": [],
+				"images": [],
+				"videos": [],
+				"songs": [],
+				"influencers": []
+			},
+			{
+				"name": "The Rolling Stones",
+				"bio": [],
+				"images": [],
+				"videos": [],
+				"songs": [],
+				"influencers": []    
+			},
+			{
+				"name": "The Beach Boys",
+				"bio": [],
+				"images": [],
+				"videos": [],
+				"songs": [],
+				"influencers": []
+			},
+			{
+				"name": "Ray Charles",
+				"bio": [],
+				"images": [],
+				"videos": [],
+				"songs": [],
+				"influencers": []
+			}
+		]
+	},
+	"decade": {
+		"year": "1970s",
+		"artistsArray": [
+			{
+				"name": "Elton John",
+				"images": [],
+				"bio": [],
+				"images": [],
+				"videos": [],
+				"songs": [],
+				"influencers": []
+			},
+			{
+				"name": "KISS",
+				"bio": [],
+				"images": [],
+					"videos": [],
+					"songs": [],
+					"influencers": []
+			},
+			{
+				"name": "Bee Gees",
+				"bio": [],
+				"images": [],
+				"videos": [],
+				"songs": [],
+				"influencers": []
+			},
+			{
+				"name": "Earth, Wind, Fire",
+				"bio": [],
+				"images": [],
+				"videos": [],
+				"songs": [],
+				"influencers": []
+			},
+			{
+				"name": "Rod Stewart",
+				"bio": [],
+				"images": [],
+				"videos": [],
+				"songs": [],
+				"influencers": []
+			}
+		]
+	},
+	"decade": {
+		"year": "1980s",
+		"artistsArray": [
+			{
+				"name": "Madonna",
+				"bio": [],
+				"images": [],
+				"videos": [],
+				"songs": [],
+				"influencers": []
+			},
+			{
+				"name": "Prince",
+				"bio": [],
+				"images": [],
+				"videos": [],
+				"songs": [],
+				"influencers": []
+			},
+			{
+				"name": "Michael Jackson",
+				"bio": [],
+				"images": [],
+				"videos": [],
+				"songs": [],
+				"influencers": []
+			},
+			{
+				"name": "Billy Joel",
+				"bio": [],
+				"images": [],
+				"videos": [],
+				"songs": [],
+				"influencers": []
+			},
+			{
+				"name": "The Police",
+				"bio": [],
+				"images": [],
+				"videos": [],
+				"songs": [],
+				"influencers": []
+			}
+		]
+	},
+	"decade": {
+		"year": "1990s",
+		"artistsArray": [
+			{
+				"name": "Nirvana",
+				"bio": [],
+				"images": [],
+				"videos": [],
+				"songs": [],
+				"influencers": []
+			},
+			{
+				"name": "U2",
+				"bio": [],
+				"images": [],
+				"videos": [],
+				"songs": [],
+				"influencers": []
+			},
+			{
+				"name": "Backstreet Boys",
+				"bio": [],
+				"images": [],
+				"videos": [],
+				"songs": [],
+				"influencers": []
+			},
+			{
+				"name": "Pearl Jam",
+				"bio": [],
+				"images": [],
+				"videos": [],
+				"songs": [],
+				"influencers": []
+			},
+			{
+				"name": "The Spice Girls",
+				"bio": [],
+				"images": [],
+				"videos": [],
+				"songs": [],
+				"influencers": []
+			}
+		]
+	},
+	"decade": {
+		"year": "2000s",
+		"artistsArray": [
+			{
+				"name": "Green Day",
+				"bio": [],
+				"images": [],
+				"videos": [],
+				"songs": [],
+				"influencers": []
+			},
+			{
+				"name": "Red Hot Chili Peppers",
+				"bio": [],
+				"images": [],
+				"videos": [],
+				"songs": [],
+				"influencers": []
+			},
+			{
+				"name": "The Black Keys",
+				"bio": [],
+				"images": [],
+				"videos": [],
+				"songs": [],
+				"influencers": []
+			},
+			{
+				"name": "Beyonce",
+				"bio": [],
+				"images": [],
+				"videos": [],
+				"songs": [],
+				"influencers": []
+			},
+			{
+				"name": "Justin Timberlake",
+				"bio": [],
+				"images": [],
+				"videos": [],
+				"songs": [],
+				"influencers": []
+			}
+		]
+	},
+	"decade": {
+		"year": "2010s",
+		"artistsArray": [
+			{
+				"name": "Adele",
+				"bio": [],
+				"images": [],
+				"videos": [],
+				"songs": [],
+				"influencers": []
+			},
+			{
+				"name": "Lady Gaga",
+				"bio": [],
+				"images": [],
+				"videos": [],
+				"songs": [],
+				"influencers": []
+			},
+			{
+				"name": "Rihanna",
+				"bio": [],
+				"images": [],
+				"videos": [],
+				"songs": [],
+				"influencers": []
+			},
+			{
+				"name": "Taylor Swift",
+				"bio": [],
+				"images": [],
+				"videos": [],
+				"songs": [],
+				"influencers": []
+			},
+			{
+				"name": "Ed Sheeran",
+				"bio": [],
+				"images": [],
+				"videos": [],
+				"songs": [],
+				"influencers": []
+			}
+		]
+	}
 }
-
 module.exports = artists;
