cc937a81b4044f82f548e47bb625d134ecd28bec
KYLE PETERSON (RIT Student) <kep6634@cias-1303-10.main.ad.rit.edu>
2016-04-23 14:32:39 -0400

    Test Images

--- artists.js
+++ artists.js
@@ -6,7 +6,324 @@ var artists = {
 				{
 					"name": "Elvis Presley",
 					"bio": [],
+					"images": [var artists = {
+	"decade": [
+		{
+			"year": "1950s",
+			"artistsArray": [
+				{
+					"name": "Elvis Presley",
+					"bio": [],
+					"images": ["https://lh6.googleusercontent.com/bOBjbB8x-bTVz0RCawmRQ3ja5q0MqytYMTj-KbTOZ2pSslyyNfn9-RWxirOxwlx2AVgk_w=w1044-h917"],
+					"videos": [],
+					"songs": [],
+					"influencers": []
+				},
+				{
+					"name": "Frank Sinatra",
+					"images": ["https://lh6.googleusercontent.com/F7q-0MGFuJgCvmDrRaKj-uVI_G6BxDZlIypbrl6IJ8I8cakDF-qG3kQffsJHV2Krw3SN7Q=w1059-h881"],
+					"videos": [],
+					"influencers": []
+				},
+				{
+					"name": "Nat King Cole",
+					"bio": [],
+					"images": ["https://lh3.googleusercontent.com/zKC9RsV7nvaT0DIgOiPN3pZvRi6J7ZYPG56fmVJfrjBedmlwPnmIiIwE_yvR_6vMb_Pj0w=w1059-h917"],
+					"videos": [],
+					"songs": [],
+					"influencers": []
+				},
+				{
+					"name": "Johnny Cash",
+					"bio": [],
+					"images": ["https://lh6.googleusercontent.com/0uvXnFIhMHCKvzMUy8MYoNDTJkTCxbTyo_i6CDXr067TnCh8wgG4rue4F-RUZhdSJNoDQQ=w1059-h917"],
+					"videos": [],
+					"songs": [],
+					"influencers": []
+				},
+				{
+					"name": "Ella Fitzgerald",
+					"bio": [],
+					"images": ["https://lh3.googleusercontent.com/YgINH6oqsLMb6g-bfdmzCUsCxaX2QdEP1RziGFlV0ogS22n3mbfJbCgKcnDoB4FJi9x8tg=w1059-h917"],
+					"videos": [],
+					"songs": [],
+					"influencers": []
+				}
+			]
+		},
+		{
+			"year": "1960s",
+			"artistsArray": [
+				{
+					"name": "The Beatles",
+					"bio": [],
 					"images": [],
+					"videos": [],
+					"songs": [],
+					"influencers": []
+				},
+				{
+					"name": "The Supremes",
+					"bio": [],
+					"images": [],
+					"videos": [],
+					"songs": [],
+					"influencers": []
+				},
+				{
+					"name": "The Rolling Stones",
+					"bio": [],
+					"images": [],
+					"videos": [],
+					"songs": [],
+					"influencers": []    
+				},
+				{
+					"name": "The Beach Boys",
+					"bio": [],
+					"images": [],
+					"videos": [],
+					"songs": [],
+					"influencers": []
+				},
+				{
+					"name": "Ray Charles",
+					"bio": [],
+					"images": [],
+					"videos": [],
+					"songs": [],
+					"influencers": []
+				}
+			]
+		},
+		{
+			"year": "1970s",
+			"artistsArray": [
+				{
+					"name": "Elton John",
+					"images": [],
+					"bio": [],
+					"images": [],
+					"videos": [],
+					"songs": [],
+					"influencers": []
+				},
+				{
+					"name": "KISS",
+					"bio": [],
+					"images": [],
+						"videos": [],
+						"songs": [],
+						"influencers": []
+				},
+				{
+					"name": "Bee Gees",
+					"bio": [],
+					"images": [],
+					"videos": [],
+					"songs": [],
+					"influencers": []
+				},
+				{
+					"name": "Earth, Wind, Fire",
+					"bio": [],
+					"images": [],
+					"videos": [],
+					"songs": [],
+					"influencers": []
+				},
+				{
+					"name": "Rod Stewart",
+					"bio": [],
+					"images": [],
+					"videos": [],
+					"songs": [],
+					"influencers": []
+				}
+			]
+		},
+		{
+			"year": "1980s",
+			"artistsArray": [
+				{
+					"name": "Madonna",
+					"bio": [],
+					"images": [],
+					"videos": [],
+					"songs": [],
+					"influencers": []
+				},
+				{
+					"name": "Prince",
+					"bio": [],
+					"images": [],
+					"videos": [],
+					"songs": [],
+					"influencers": []
+				},
+				{
+					"name": "Michael Jackson",
+					"bio": [],
+					"images": [],
+					"videos": [],
+					"songs": [],
+					"influencers": []
+				},
+				{
+					"name": "Billy Joel",
+					"bio": [],
+					"images": [],
+					"videos": [],
+					"songs": [],
+					"influencers": []
+				},
+				{
+					"name": "The Police",
+					"bio": [],
+					"images": [],
+					"videos": [],
+					"songs": [],
+					"influencers": []
+				}
+			]
+		},
+		{
+			"year": "1990s",
+			"artistsArray": [
+				{
+					"name": "Nirvana",
+					"bio": [],
+					"images": [],
+					"videos": [],
+					"songs": [],
+					"influencers": []
+				},
+				{
+					"name": "U2",
+					"bio": [],
+					"images": [],
+					"videos": [],
+					"songs": [],
+					"influencers": []
+				},
+				{
+					"name": "Backstreet Boys",
+					"bio": [],
+					"images": [],
+					"videos": [],
+					"songs": [],
+					"influencers": []
+				},
+				{
+					"name": "Pearl Jam",
+					"bio": [],
+					"images": [],
+					"videos": [],
+					"songs": [],
+					"influencers": []
+				},
+				{
+					"name": "The Spice Girls",
+					"bio": [],
+					"images": [],
+					"videos": [],
+					"songs": [],
+					"influencers": []
+				}
+			]
+		},
+		{
+			"year": "2000s",
+			"artistsArray": [
+				{
+					"name": "Green Day",
+					"bio": [],
+					"images": [],
+					"videos": [],
+					"songs": [],
+					"influencers": []
+				},
+				{
+					"name": "Red Hot Chili Peppers",
+					"bio": [],
+					"images": [],
+					"videos": [],
+					"songs": [],
+					"influencers": []
+				},
+				{
+					"name": "The Black Keys",
+					"bio": [],
+					"images": [],
+					"videos": [],
+					"songs": [],
+					"influencers": []
+				},
+				{
+					"name": "Beyonce",
+					"bio": [],
+					"images": [],
+					"videos": [],
+					"songs": [],
+					"influencers": []
+				},
+				{
+					"name": "Justin Timberlake",
+					"bio": [],
+					"images": [],
+					"videos": [],
+					"songs": [],
+					"influencers": []
+				}
+			]
+		},
+		{
+			"year": "2010s",
+			"artistsArray": [
+				{
+					"name": "Adele",
+					"bio": [],
+					"images": [],
+					"videos": [],
+					"songs": [],
+					"influencers": []
+				},
+				{
+					"name": "Lady Gaga",
+					"bio": [],
+					"images": [],
+					"videos": [],
+					"songs": [],
+					"influencers": []
+				},
+				{
+					"name": "Rihanna",
+					"bio": [],
+					"images": [],
+					"videos": [],
+					"songs": [],
+					"influencers": []
+				},
+				{
+					"name": "Taylor Swift",
+					"bio": [],
+					"images": [],
+					"videos": [],
+					"songs": [],
+					"influencers": []
+				},
+				{
+					"name": "Ed Sheeran",
+					"bio": [],
+					"images": [],
+					"videos": [],
+					"songs": [],
+					"influencers": []
+				}
+			]
+		}
+	]
+}],
 					"videos": [],
 					"songs": [],
 					"influencers": []
@@ -260,7 +577,7 @@ var artists = {
 					"influencers": []
 				},
 				{
-					"name": "Justin Timberlake",
+					"name": "Rihanna",
 					"bio": [],
 					"images": [],
 					"videos": [],
