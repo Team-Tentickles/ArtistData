04adf68d5cdff08e1ae434194da3352bf6f17918
KYLE PETERSON (RIT Student) <kep6634@cias-1303-10.main.ad.rit.edu>
2016-04-23 15:57:10 -0400

    Test Images

--- artists.js
+++ artists.js
@@ -6,7 +6,7 @@ var artists = {
 				{
 					"name": "Elvis Presley",
 					"bio": [],
-					"images": ["..artistImages/Elvis_Presley/Elvis_Presley.jpg"],
+					"images": ["../artistImages/Elvis_Presley/Elvis_Presley.jpg"],
 					"videos": [],
 					"songs": [],
 					"influencers": []
