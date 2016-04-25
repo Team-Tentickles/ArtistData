603f9d75a8630d1b803565b7a372a178c0b6af7c
KyleEvan <kep6634@rit.edu>
2016-04-25 12:23:56 -0400

    Test Images

--- artists.js
+++ artists.js
@@ -6,7 +6,7 @@ var artists = {
 				{
 					"name": "Elvis Presley",
 					"bio": [],
-					"images": ["file:///../public/artistImages/Elvis_Presley/Elvis_Presley.jpg"],
+					"images": ["artistImages/Elvis_Presley/Elvis_Presley.jpg"],
 					"videos": [],
 					"songs": [],
 					"influencers": []
